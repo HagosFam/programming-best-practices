80d5533c32629b9f909575812f0f66fc5056f33d
hagos <111482114+Kytsync@users.noreply.github.com>
2023-05-07 00:03:37 -0500

    generics

---
+++ Best-Practices/src/Generics/Generics.java
@@ -0,0 +1,6 @@
+package Generics;
+
+// generics doesnt work with primitive types like int, double or float
+// generics work with non primitive like String, Integers, Double ...
+public class Generics {
+}
